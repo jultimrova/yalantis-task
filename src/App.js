0ed0eadc14b75fedf09eb29251b2bf781f977814
jultimrova@gmail.com <splntrs019>
2020-11-15 17:39:50 +0200

    employees sorting by last name

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from "react";
 import Employees from "./components/Employees";
 
 function App() {
-  return (
-    <div className="App">
-      <Employees />
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Employees/>
+        </div>
+    );
 }
 
 export default App;
